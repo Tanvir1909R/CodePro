c5c98d05d5fa22aeae85a34f6c51074815d7d3eb
Tanvir1909R <trcr719@gmail.com>
2022-10-25 00:15:32 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/header/Header'
+import Home from './pages/home/Home'
+import Courses from './pages/courses/Courses'
+import Faq from './pages/faq/Faq'
+import Blog from './pages/Blog'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+      <main>
+        <Routes>
+          <Route path='/' element={ <Home/> } />
+          <Route path='/courses' element={ <Courses/> } />
+          <Route path='/faq' element={ <Faq/> } />
+          <Route path='/blog' element={ <Blog /> } />
+        </Routes>
+      </main>
+    </>
   )
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './header.css'
 
 const Header = () => {
   return (
     <header>
-        
+        <div className="container">
+          <div className="headerWrapper">
+            <div className="logo">Code.Pro</div>
+            <nav>
+              <NavLink to='/'>Home</NavLink>
+              <NavLink to='/courses'>Courses</NavLink>
+              <NavLink to='/faq'>FAQ</NavLink>
+              <NavLink to='/blog'>Blog</NavLink>
+            </nav>
+            <div className="profile">
+              <div className="profileImg">
+                <img src="" alt="" />
+              </div>
+              <div className="themeBtn">
+                  <div className="line">
+                    <div className="switch">G</div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </header>
   )
 }
